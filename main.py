d9306945412f74c607ee87b2bc3035946a77df8a
Smokey <105644067+smokeyisdev@users.noreply.github.com>
2022-11-03 10:08:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,8 @@ st.write(pd.DataFrame({
 
 x = st.slider('x')  # 👈 this is a widget
 st.write(x, 'squared is', x * x)
+
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
