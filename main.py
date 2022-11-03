07b9947d44460b5b68d0115dff516614d66420d1
Smokey <105644067+smokeyisdev@users.noreply.github.com>
2022-11-03 10:21:41 +0000

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ import pandas as pd
 import numpy as np
 
 
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
 if st.checkbox('Show dataframe'):
     chart_data = pd.DataFrame(
        np.random.randn(20, 3),
