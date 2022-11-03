154e61c3286c8d0b8183148e64c9c7e760f354c4
Smokey <105644067+smokeyisdev@users.noreply.github.com>
2022-11-03 10:07:12 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ Here's our first attempt at using data to create a table:
 """
 
 import streamlit as st
+import pandas as pd
 
 
 st.write("Hello World!*")
 st.write(st.slider("what"))
+
+
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
